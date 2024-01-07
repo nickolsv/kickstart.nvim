dd0fd69f3f557b9b490945a4cb9434369051a9d0
nickolsv <nickols1234@gmail.com>
2024-01-07 21:09:04 +0200

    Revert tmux statusline

--- init.lua
+++ init.lua
@@ -353,58 +353,58 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 })
 
 -- [[ Hide/Show tmux bar ]]
-local tmux_group = vim.api.nvim_create_augroup('TmuxStatus', { clear = true })
-vim.api.nvim_create_autocmd('VimEnter', {
-  callback = function()
-    vim.fn.system {
-      'tmux',
-      'set',
-      'status',
-      'off'
-    }
-  end,
-  group = tmux_group,
-  pattern = '*',
-})
-
-vim.api.nvim_create_autocmd('VimLeave', {
-  callback = function()
-    vim.fn.system {
-      'tmux',
-      'set',
-      'status',
-      'on'
-    }
-  end,
-  group = tmux_group,
-  pattern = '*',
-})
-
-vim.api.nvim_create_autocmd('VimResume', {
-  callback = function()
-    vim.fn.system {
-      'tmux',
-      'set',
-      'status',
-      'off'
-    }
-  end,
-  group = tmux_group,
-  pattern = '*',
-})
-
-vim.api.nvim_create_autocmd('VimSuspend', {
-  callback = function()
-    vim.fn.system {
-      'tmux',
-      'set',
-      'status',
-      'on'
-    }
-  end,
-  group = tmux_group,
-  pattern = '*',
-})
+-- local tmux_group = vim.api.nvim_create_augroup('TmuxStatus', { clear = true })
+-- vim.api.nvim_create_autocmd('VimEnter', {
+--   callback = function()
+--     vim.fn.system {
+--       'tmux',
+--       'set',
+--       'status',
+--       'off'
+--     }
+--   end,
+--   group = tmux_group,
+--   pattern = '*',
+-- })
+--
+-- vim.api.nvim_create_autocmd('VimLeave', {
+--   callback = function()
+--     vim.fn.system {
+--       'tmux',
+--       'set',
+--       'status',
+--       'on'
+--     }
+--   end,
+--   group = tmux_group,
+--   pattern = '*',
+-- })
+--
+-- vim.api.nvim_create_autocmd('VimResume', {
+--   callback = function()
+--     vim.fn.system {
+--       'tmux',
+--       'set',
+--       'status',
+--       'off'
+--     }
+--   end,
+--   group = tmux_group,
+--   pattern = '*',
+-- })
+--
+-- vim.api.nvim_create_autocmd('VimSuspend', {
+--   callback = function()
+--     vim.fn.system {
+--       'tmux',
+--       'set',
+--       'status',
+--       'on'
+--     }
+--   end,
+--   group = tmux_group,
+--   pattern = '*',
+-- })
 
 -- Treat JSON files as JSONc (allow comments)
 vim.api.nvim_command([[
