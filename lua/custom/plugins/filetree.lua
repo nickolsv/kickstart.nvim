9e1a54e29fa00bb581c95dc5692a1afb227c2e08
nickolsv <nickols1234@gmail.com>
2024-01-13 23:07:14 +0200

    Add code snippet to not open empty buffer alongside neotree when calling nvim with a directory as an argument

--- lua/custom/plugins/filetree.lua
+++ lua/custom/plugins/filetree.lua
@@ -8,7 +8,17 @@ return {
     "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
     "MunifTanjim/nui.nvim",
   },
+  init = function()
+    if vim.fn.argc(-1) == 1 then
+      local stat = vim.loop.fs_stat(vim.fn.argv(0))
+      if stat and stat.type == "directory" then
+        require("neo-tree").setup({ filesystem = { hijack_netrw_behavior = "open_current", }, })
+      end
+    end
+  end,
   config = function()
-    require('neo-tree').setup {}
+    require('neo-tree').setup {
+
+    }
   end,
 }
