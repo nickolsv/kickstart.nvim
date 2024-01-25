33d5d16a9b270d649954561d3b3e7134f19bb158
nickolsv <nickols1234@gmail.com>
2024-01-26 01:57:46 +0200

    add code rename lsp keymap

--- lua/nvim-keymaps.lua
+++ lua/nvim-keymaps.lua
@@ -64,6 +64,7 @@ LSP_on_attach = function(_, bufnr)
 
   nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
   nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+  nmap('<leader>cr', vim.lsp.buf.rename, '[C]ode [R]ename')
 
   nmap('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
   nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
